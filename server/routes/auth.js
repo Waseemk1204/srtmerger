8bb224bd6cce3092db26a0adc5d997527f40b965
Waseem Khan <waseem@Waseems-MacBook-Pro.local>
2025-11-27 05:47:44 +0530

    Fix Vercel crash: Replace bcrypt with bcryptjs

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { ObjectId } = require('mongodb');
 const { getDB } = require('../config/db');
